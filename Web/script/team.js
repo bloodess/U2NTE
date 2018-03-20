c71ec2f50c54710241e7a7d8c9586149a42cdd51
Unknown <clement.caillaud97@gmail.com>
2018-03-20 15:48:44 +0100

    Animation cards

--- Web/script/team.js
+++ Web/script/team.js
@@ -1,16 +1,15 @@
 $(function()
 {
-  $(".card-text").hide();
-  $(".list-group").hide();
+  //On cache la partie "compétences"
+  $(".list-group").animate({height: "toggle"},0);
 
+  //Lors du clic sur le bouton, on affiche la partie "compétences"
   $(".btn").click(function()
   {
-    $(this).parent().parent().find(".card-body").find(".card-text").toggle();
-    $(this).parent().parent().find(".card-body").find(".list-group").toggle();
-
-    if($(this).is(":visible"))
-    {
-      console.log("visible");
-    }
+    //Le div compétences de la fiche actuelle
+    var competences = $(this).parent().parent().find(".card-body").find(".list-group");
+    //Animation pour afficher la div et tourner le bouton
+    competences.animate({height: "toggle"},250);
+    $(this).find("span").toggleClass("show-div");
   });
 })
